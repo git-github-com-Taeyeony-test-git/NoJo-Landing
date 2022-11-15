817e16e30f50c80a2862ae156a213e3bbb4467f7
JungMin <cvy5375@naver.com>
2022-11-15 17:41:26 +0900

    JungMin

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, jsonify
-
 app = Flask(__name__)
-from pymongo import MongoClient
 
+
+from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.c4jurvw.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 
 @app.route('/')
@@ -14,29 +15,30 @@ def home():
 
 @app.route("/asher")
 def asher():
-    return render_template("asher-landing.html");
-
+    return render_template('asher-landing.html')
 
 @app.route("/JungMin")
 def JungMin():
-    return render_template('Jungmin.html')
+    return render_template('JungMin.html')
 
-
-@app.route("/api/asher/comment", methods=["POST"])
+@app.route("/api/JungMin/comment", methods=["POST"])
 def comment_post():
     name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
+
     doc = {
         'name': name_receive,
         'comment': comment_receive
     }
-    db.asher_comment.insert_one(doc)
+
+    db.JungMin_comment.insert_one(doc)
+
     return jsonify({'msg': '등록완료'})
 
-
-@app.route("/api/asher/comment", methods=["GET"])
+@app.route("/api/JungMin/comment", methods=["GET"])
 def comment_get():
-    comment_list = list(db.asher_comment.find({}, {'_id': False}))
+
+    comment_list = list(db.JungMin_comment.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
 
 
